b36420e7d571b6572786bb4e83142c1e9e1a7080
buyi <9440979@qq.com>
2020-05-25 19:34:36 +0800

    U 增加seleninum支持

--- src/test/java/com/puffer/shopify/spider/processor/AmazonPageProcessorTest.java
+++ src/test/java/com/puffer/shopify/spider/processor/AmazonPageProcessorTest.java
@@ -10,6 +10,7 @@ import com.puffer.shopify.vo.ProductVO;
 import org.assertj.core.util.Lists;
 import org.testng.annotations.Test;
 import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.downloader.selenium.SeleniumDownloader;
 import us.codecraft.webmagic.selector.PlainText;
 import us.codecraft.webmagic.selector.Selectable;
 
@@ -41,6 +42,23 @@ public class AmazonPageProcessorTest extends AbstractTest {
                 .addPipeline(amazonPipeline)
                 //开启5个线程抓取
                 .thread(5)
+                //启动爬虫
+                .run();
+    }
+
+
+
+
+    @Test
+    public void testProcess2() {
+
+        //        String url = "https://www.amazon.com/gp/new-releases/kitchen/9302388011/ref=zg_bsnr_pg_1?ie=UTF8&pg=1";
+        String url = "https://www.amazon.com/Coffee-Remember-Graduation-Quarantine-Ceramic/dp/B08742Y2XN/ref=nav_signin?_encoding=UTF8&psc=1&refRID=6WT78YTWCGCQSHWMF94E&";
+        Spider.create(new AmazonPageProcessor())
+                .addUrl(url)
+                .addPipeline(amazonPipeline)
+                //开启5个线程抓取
+                .thread(5).setDownloader(new SeleniumDownloader("/Users/yihua/Downloads/chromedriver"))
                 //启动爬虫
                 .run();
     }
